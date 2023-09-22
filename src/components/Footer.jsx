8d706999aea50f20707d3d9aca8159fe23917f4e
zzaferr <zaferdemirelgsb@gmail.com>
2023-09-22 22:44:28 +0300

    project is uploaded

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='socialMedia'>
+            <p> &copy; 2023 burger06.com </p>
+           
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import BannerImage from '../assets/banner.jpg'
+const Home = () => {
+  return (
+    <div className='home' style={{backgroundImage:`url(${BannerImage})`}}>
+        <div className='headerContainer'>
+            <h1>Burger 26</h1>
+            <p>Delicious Hamburgers<br/>with 26 spicy mixed</p>
+            <Link to="/menu"><button>Order Now</button></Link>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
